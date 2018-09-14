5d91bcc6e2fc05855b331ef046c0a2ee17bae403
Stephen E Slevinski Jr <slevin@signpuddle.net>
2018-09-14 15:30:36 -0500

    v3.0.0-alpha.1

---
+++ front/data/js/interface-sp3.js
@@ -0,0 +1,201 @@
+interfaces["default"] = interfaces["default"]?interfaces["default"]:{}
+interfaces["default"]["sp3"] = {
+  "system.signpuddle3.title": {
+    "message": "SignPuddle $1",
+    "description": "Title of SignPuddle.",
+    "icon": "umbrella"
+  },
+  "system.signpuddle3.short": {
+    "message": "SP$1",
+    "description": "Short title of SignPuddle."
+  },
+  "system.buttons.edit": {
+    "message": "Edit",
+    "description": "Generic edit button"
+  },
+  "system.buttons.open": {
+    "message": "Open",
+    "description": "Generic open button"
+  },
+  "system.buttons.remove": {
+    "message": "Remove",
+    "description": "Generic remove button"
+  },
+  "system.buttons.clear": {
+    "message": "Clear",
+    "description": "Generic clear button"
+  },
+  "system.buttons.cancel": {
+    "message": "Cancel",
+    "description": "Generic cancel button"
+  },
+  "system.buttons.initial": {
+    "message": "Initial",
+    "description": "Generic initial button"
+  },
+  "system.buttons.save": {
+    "message": "Save",
+    "description": "Generic save button"
+  },
+  "system.buttons.restore": {
+    "message": "Restore",
+    "description": "Generic restore button"
+  },
+  "system.buttons.forget": {
+    "message": "Forget",
+    "description": "Generic forget button"
+  },
+  "system.buttons.error": {
+    "message": "Error",
+    "description": "Generic error button"
+  },
+  "system.buttons.select": {
+    "message": "Select",
+    "description": "Select item."
+  },
+  "system.buttons.selected": {
+    "message": "Selected",
+    "description": "Selected item."
+  },
+  "system.buttons.more": {
+    "message": "More",
+    "description": "Show more information."
+  },
+  "settings.main.title": {
+    "message": "Settings",
+    "description": "Settings title."
+  },
+  "settings.server.title": {
+    "message": "Server",
+    "description": "Server Settings title."
+  },
+  "settings.country.title": {
+    "message": "Country",
+    "description": "Country Settings title."
+  },
+  "system.buttons.check": {
+    "message": "Check",
+    "description": "Salt not available for server."
+  },
+  "system.buttons.checked": {
+    "message": "Ready",
+    "description": "Salt available for server."
+  },
+  "settings.server.name": {
+    "message": "Server Name",
+    "description": "Label for server name"
+  },
+  "settings.server.url": {
+    "message": "Server URL",
+    "description": "Label for server url"
+  },
+  "settings.server.connect": {
+    "message": "Connect",
+    "description": "Connect to server."
+  },
+  "settings.server.disconnect": {
+    "message": "Disconnect",
+    "description": "Disconnect from server."
+  },
+  "settings.server.reset": {
+    "message": "Reset Connection",
+    "description": "Reset Server connection."
+  },
+  "settings.server.name": {
+    "message": "Server Name",
+    "description": "Label for server name"
+  },
+  "settings.server.url": {
+    "message": "Server URL",
+    "description": "Label for server url"
+  },
+  "settings.interfaces.title": {
+    "message": "Interfaces",
+    "description": "Interfaces title."
+  },
+  "settings.dictionarys.title": {
+    "message": "Dictionaries",
+    "description": "Dictionary Settings title."
+  },
+  "settings.literatures.title": {
+    "message": "Literatures",
+    "description": "Literatures Settings title."
+  },
+  "settings.alphabets.title": {
+    "message": "Alphabets",
+    "description": "Alphabets Settings title."
+  },
+  "settings.fingerspells.title": {
+    "message": "Fingerspells",
+    "description": "Fingerspells Settings title."
+  },
+  "user.profile.title": {
+    "message": "User Profile",
+    "description": "Profile title"
+  },
+  "user.profile.username": {
+    "message": "Username",
+    "description": "Label for user name"
+  },
+  "user.profile.email": {
+    "message": "Email",
+    "description": "Label for user email."
+  },
+  "user.profile.password": {
+    "message": "Password",
+    "description": "Label for user password."
+  },
+  "user.profile.button": {
+    "message": "Update Profile",
+    "description": "Text of button."
+  },
+  "user.login.title": {
+    "message": "Login",
+    "description": "Login title."
+  },
+  "user.login.button": {
+    "message": "Login",
+    "description": "Text of button."
+  },
+  "user.reset.title": {
+    "message": "User Account Reset",
+    "description": "Login title."
+  },
+  "user.reset.button": {
+    "message": "Reset Password",
+    "description": "Text of button."
+  },
+  "user.register.title": {
+    "message": "Register",
+    "description": "Register title."
+  },
+  "user.register.button": {
+    "message": "Register",
+    "description": "Text of button."
+  },
+  "system.response.status": {
+    "message": "Status",
+    "description": "Response status code"
+  },
+  "system.response.headers": {
+    "message": "Headers",
+    "description": "Response code headers"
+  },
+  "system.server.problem": {
+    "message": "ERROR: Server problem",
+    "description": "error message when server has a problem."
+  },
+  "system.server.success": {
+    "message": "SUCCESS: Server response",
+    "description": "message when server responds successfully."
+  },
+  "interface.defaults.description": {
+    "message": "Default messages for SignPuddle 3"
+  },
+  "interface.qqq.description": {
+    "message": "Message key values"
+  },
+  "system.buttons.download": {
+    "message": "Download"
+  }
+}
